f49541c4e30efe7c35f2b80e0e4544ffad50bb79
lucaspod <lukasalvespod1@gmail.com>
2019-01-21 20:55:15 -0300

    criando componentes de detalhe de restaurante

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { HomeComponent } from './home/home.component';
 import {Routes} from '@angular/router';
 import { AboutComponent } from './about/about.component';
@@ -5,6 +6,7 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 export const ROUTES: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
   {path: 'restaurants', component: RestaurantsComponent},
+  {path: 'restaurants/:id', component: RestaurantDetailComponent},
+  {path: 'about', component: AboutComponent},
 ];
