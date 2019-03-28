b4c299aecd629f763e35ef4503ba2881347d711f
José Lucas <lukasalvespod1@gmail.com>
2019-03-28 01:52:05 -0300

    rotas filhas para avaliacao e menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { HomeComponent } from './home/home.component';
 import {Routes} from '@angular/router';
@@ -7,6 +9,11 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 export const ROUTES: Routes = [
   {path: '', component: HomeComponent},
   {path: 'restaurants', component: RestaurantsComponent},
-  {path: 'restaurants/:id', component: RestaurantDetailComponent},
+  {path: 'restaurants/:id', component: RestaurantDetailComponent,
+    children:[
+      {path: '', redirectTo: 'menu', pathMatch: 'full'},
+      {path: 'menu', component: MenuComponent},
+      {path: 'reviews', component: ReviewsComponent}
+    ]},
   {path: 'about', component: AboutComponent},
 ];
