dd3554200f4a2c966ab362502311d1a091566c07
José Lucas <lukasalvespod1@gmail.com>
2019-03-28 14:41:59 -0300

    implementado o componente de avaliacoes

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,29 +1,37 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
 
-import { Restaurant } from './restaurant/restaurant.model';
+import { Restaurant } from "./restaurant/restaurant.model";
 
-import { MEAT_API } from '../app.api';
+import { MEAT_API } from "../app.api";
 
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 
-import { ErrorHandler } from '../app.error-handler';
+import { ErrorHandler } from "../app.error-handler";
 
 @Injectable()
 export class RestaurantsService {
-
   constructor(private http: Http) {}
 
   restaurants(): Observable<Restaurant[]> {
-    return this.http.get(`${MEAT_API}/restaurants`)
+    return this.http
+      .get(`${MEAT_API}/restaurants`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError);
   }
 
-  restaurantById(id: string): Observable<Restaurant>{
-    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+  restaurantById(id: string): Observable<Restaurant> {
+    return this.http
+      .get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
+  }
+
+  reviewsOfRestaurant(id: string): Observable<any> {
+    return this.http
+      .get(`${MEAT_API}/restaurants/${id}/reviews`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError);
   }
